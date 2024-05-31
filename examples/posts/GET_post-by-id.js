b310a16d2b7e5adb1ead75c2d9f103fbf22c014d
sun2ii <benbasuni1@gmail.com>
2024-05-31 10:31:01 -0500

    Update GET_post-by-id.js

--- examples/posts/GET_post-by-id.js
+++ examples/posts/GET_post-by-id.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const BeehiivSDK = require('../../src/beehiiv-sdk');
 const sdk = new BeehiivSDK();
-const postId = 'post_7125a615-2985-4169-83f3-353b4dd24348';
+const postId = 'your-post-id';
 
 (async () => {
   try {
